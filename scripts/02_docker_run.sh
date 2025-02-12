7ba667476e6b8e982652852eb18a13a2ef16a6da
Nathan Hui <nthui@ucsd.edu>
2025-02-11 18:28:03 -0800

    fix: Fixes volume mounts

--- scripts/02_docker_run.sh
+++ scripts/02_docker_run.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+mkdir -p results
+mkdir -p configs
 docker run --rm -it --gpus=all -e NVIDIA_DRIVER_CAPABILITIES=all \
     -v ./data:/home/ubuntu/raw_data:ro \
-    -v ./results:/home/ubuntu/Results:rw \
-    -v ./configs:/home/ubuntu/Configs:rw \
     -v ./lens_cal:/home/ubuntu/calibrations:ro \
     -v ./scripts:/home/ubuntu/scripts:ro \
+    -v ./results:/home/ubuntu/Results:rw \
+    -v ./configs:/home/ubuntu/Configs:rw \
     --shm-size=10.24gb `whoami`/fishsense-lite
