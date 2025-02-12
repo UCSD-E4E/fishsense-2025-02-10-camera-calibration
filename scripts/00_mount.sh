77b656f675af1e8224e3bcd35d4036be601aedad
Nathan Hui <nthui@ucsd.edu>
2025-02-11 18:11:35 -0800

    chore: Adds lens cal

--- scripts/00_mount.sh
+++ scripts/00_mount.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 mkdir ./data
 sudo mount -t cifs -o credentials=$E4E_NAS_CREDS,uid=`whoami`,ro "\\\\e4e-nas.ucsd.edu\fishsense_data\2025.02.10.FishSense.NathanPool" ./data
+
+mkdir ./lens_cal
+mkdir ./lens_cal/FSL-09D
+mkdir ./lens_cal/FSL-11D
+sudo mount -t cifs -o credentials=$E4E_NAS_CREDS,uid=`whoami`,ro "\\\\e4e-nas.ucsd.edu\fishsense\Fishsense Lite Calibration Parameters\FSL-09D" ./lens_cal/FSL-09D
+sudo mount -t cifs -o credentials=$E4E_NAS_CREDS,uid=`whoami`,ro "\\\\e4e-nas.ucsd.edu\fishsense\Fishsense Lite Calibration Parameters\FSL-11D" ./lens_cal/FSL-11D
--- scripts/99_umount.sh
+++ scripts/99_umount.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
-sudo umount ./data
-rmdir ./data
+if mount | grep '//e4e-nas.ucsd.edu/fishsense_data/2025.02.10.FishSense.NathanPool' > /dev/null; then
+    sudo umount ./data
+fi
+if mount | grep '//e4e-nas.ucsd.edu/fishsense/Fishsense Lite Calibration Parameters/FSL-09D' > /dev/null; then
+    sudo umount ./lens_cal/FSL-09D
+fi
+if mount | grep '//e4e-nas.ucsd.edu/fishsense/Fishsense Lite Calibration Parameters/FSL-11D' > /dev/null; then
+    sudo umount ./lens_cal/FSL-11D
+fi
+if [ -d './data' ]; then
+    rmdir ./data
+fi
+if [ -d './lens_cal' ]; then
+    rm -rf ./lens_cal
+fi
